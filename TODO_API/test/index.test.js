f77b87314a06cfede0751c2d87ba0083145d9a53
andrew <andr3wcarpentier@gmail.com>
2023-06-05 03:47:06 +0200

    add comments to tests + add read.me

--- TODO_API/test/index.test.js
+++ TODO_API/test/index.test.js
@@ -1,6 +1,6 @@
-const { expect } = require("chai");
-const app = require("../index");
-const request = require("supertest")(app);
+const { expect } = require("chai"); // Import the Chai testing library for assertions
+const app = require("../index"); // Import the Express application to be tested
+const request = require("supertest")(app); // Create an instance of supertest to make HTTP requests
 
 describe("TODO API", () => {
   describe("POST /addTodo", () => {
@@ -11,19 +11,20 @@ describe("TODO API", () => {
         done: false,
       };
       request
-        .post("/addTodo")
-        .send(todo)
-        .expect(200)
+        .post("/addTodo") // Send a POST request to the "/addTodo" endpoint
+        .send(todo) // Send the "todo" object as the request body
+        .expect(200) // Expect a response with HTTP status code 200 (OK)
         .end((err, res) => {
-          expect(err).to.be.null;
-          expect(JSON.parse(res.text)).to.have.property("id");
+          // Callback function executed when the request is complete
+          expect(err).to.be.null; // Assert that the "err" parameter is null (no error occurred)
+          expect(JSON.parse(res.text)).to.have.property("id"); // Assert that the response body has a "id" property
           expect(JSON.parse(res.text)).to.have.property(
             "content",
             todo.content
-          );
-          expect(JSON.parse(res.text)).to.have.property("edit", todo.done);
-          expect(JSON.parse(res.text)).to.have.property("done", todo.done);
-          done();
+          ); // Assert that the response body has a "content" property with the value of the sent "todo" object
+          expect(JSON.parse(res.text)).to.have.property("edit", todo.done); // Assert that the response body has a "edit" property with the value of the sent "todo" object
+          expect(JSON.parse(res.text)).to.have.property("done", todo.done); // Assert that the response body has a "done" property with the value of the sent "todo" object
+          done(); // Call done() to indicate that the test is complete
         });
     });
   });
@@ -31,12 +32,13 @@ describe("TODO API", () => {
   describe("GET /getTodos", () => {
     it("return a list of todos", (done) => {
       request
-        .get("/getTodos")
-        .expect(200)
+        .get("/getTodos") // Send a GET request to the "/getTodos" endpoint
+        .expect(200) // Expect a response with HTTP status code 200 (OK)
         .end((err, res) => {
-          expect(err).to.be.null;
-          expect(JSON.parse(res.text)).to.be.an("array");
-          done();
+          // Callback function executed when the request is complete
+          expect(err).to.be.null; // Assert that the "err" parameter is null (no error occurred)
+          expect(JSON.parse(res.text)).to.be.an("array"); // Assert that the response body is an array
+          done(); // Call done() to indicate that the test is complete
         });
     });
   });
@@ -45,15 +47,16 @@ describe("TODO API", () => {
     it("delete a todo", (done) => {
       const todo = { id: 56 };
       request
-        .post("/deleteTodo")
-        .send(todo)
-        .expect(200)
+        .post("/deleteTodo") // Send a POST request to the "/deleteTodo" endpoint
+        .send(todo) // Send the "todo" object as the request body
+        .expect(200) // Expect a response with HTTP status code 200 (OK)
         .end((err, res) => {
-          expect(err).to.be.null;
+          // Callback function executed when the request is complete
+          expect(err).to.be.null; // Assert that the "err" parameter is null (no error occurred)
           expect(JSON.parse(res.text)).to.equal(
             "Article supprimé de la base de données"
-          );
-          done();
+          ); // Assert that the response matches the expected message
+          done(); // Call done() to indicate that the test is complete
         });
     });
   });
@@ -67,19 +70,25 @@ describe("TODO API", () => {
         done: false,
       };
       request
-        .post("/modifyTodo")
-        .send(todo)
-        .expect(200)
+        .post("/modifyTodo") // Send a POST request to the "/modifyTodo" endpoint
+        .send(todo) // Send the "todo" object as the request body
+        .expect(200) // Expect a response with HTTP status code 200 (OK)
         .end((err, res) => {
-          expect(err).to.be.null;
-          expect(JSON.parse(res.text)).to.have.property("id", todo.id);
+          expect(err).to.be.null; // Assert that the "err" parameter is null (no error occurred)
+          expect(JSON.parse(res.text)).to.have.property("id", todo.id); // Assert that the response body has an "id" property with the value of the sent "todo" object
           expect(JSON.parse(res.text)).to.have.property(
             "content",
             todo.content
-          );
-          expect(JSON.parse(res.text)).to.have.property("edit", todo.edit);
-          expect(JSON.parse(res.text)).to.have.property("done", todo.done);
-          done();
+          ); // Assert that the response body has a "content" property with the value of the sent "todo" object
+          expect(JSON.parse(res.text)).to.have.property(
+            "edit",
+            todo.edit
+          ); // Assert that the response body has an "edit" property with the value of the sent "todo" object
+          expect(JSON.parse(res.text)).to.have.property(
+            "done",
+            todo.done
+          ); // Assert that the response body has a "done" property with the value of the sent "todo" object
+          done(); // Call done() to indicate that the test is complete
         });
     });
   });
