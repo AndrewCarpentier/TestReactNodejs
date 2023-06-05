9b1ebe54bde79aecc0a9ecbe0c53b31f89a81f72
AndrewCarpentier <andr3wcarpentier@gmail.com>
2023-06-05 13:07:07 +0200

    add import jest-dom in todoList

--- TODO_FRONT/src/components/AddTodo.test.js
+++ TODO_FRONT/src/components/AddTodo.test.js
@@ -31,66 +31,55 @@ describe("<AddTodo/> test", () => {
     fireEvent.click(button);
 
     // Assert the expected behavior
-    await waitFor(
-      () => {
-        expect(mockAddTodo).toHaveBeenCalledTimes(1);
-      },
-      { timeout: 2000 }
-    );
+    await waitFor(() => {
+      expect(mockAddTodo).toHaveBeenCalledTimes(1);
+    });
 
-    await waitFor(
-      () => {
-        expect(mockAddTodo).toHaveBeenCalledWith({ ...mockTodo, id: mockId });
-      },
-      { timeout: 2000 }
-    );
+    await waitFor(() => {
+      expect(mockAddTodo).toHaveBeenCalledWith({ ...mockTodo, id: mockId });
+    });
 
     // Restore the original fetch function
     global.fetch.mockRestore();
   });
 
   it("doesn't add an empty todo", async () => {
+    // Prepare mock data
     const mockTodo = {
-        content: "",
-        edit: false,
-        done: false,
-      };
-      const mockId = 0;
-      const mockAddTodo = jest.fn();
-  
-      // Mock the fetch function to return a response with the generated ID
-      jest.spyOn(global, "fetch").mockImplementation(() =>
-        Promise.resolve({
-          ok: true,
-          json: () => Promise.resolve({ ...mockTodo, id: mockId }),
-        })
-      );
-  
-      // Render the component
-      render(<AddTodo addTodo={mockAddTodo} />);
-  
-      // Interact with the UI
-      const input = screen.getByLabelText("addTodoInput");
-      const button = screen.getByLabelText("addTodoButton");
-      fireEvent.change(input, { target: { value: mockTodo.content } });
-      fireEvent.click(button);
-  
-      // Assert the expected behavior
-      await waitFor(
-        () => {
-          expect(mockAddTodo).toHaveBeenCalledTimes(0);
-        },
-        { timeout: 2000 }
-      );
-  
-      await waitFor(
-        () => {
-          expect(mockAddTodo).not.toHaveBeenCalledWith({ ...mockTodo, id: mockId });
-        },
-        { timeout: 2000 }
-      );
-  
-      // Restore the original fetch function
-      global.fetch.mockRestore();
+      content: "",
+      edit: false,
+      done: false,
+    };
+    const mockId = 0;
+    const mockAddTodo = jest.fn();
+
+    // Mock the fetch function to return a response with the generated ID
+    jest.spyOn(global, "fetch").mockImplementation(() =>
+      Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve({ ...mockTodo, id: mockId }),
+      })
+    );
+
+    // Render the component
+    render(<AddTodo addTodo={mockAddTodo} />);
+
+    // Interact with the UI
+    const input = screen.getByLabelText("addTodoInput");
+    const button = screen.getByLabelText("addTodoButton");
+    fireEvent.change(input, { target: { value: mockTodo.content } });
+    fireEvent.click(button);
+
+    // Assert the expected behavior
+    await waitFor(() => {
+      expect(mockAddTodo).toHaveBeenCalledTimes(0);
+    });
+
+    await waitFor(() => {
+      expect(mockAddTodo).not.toHaveBeenCalledWith({ ...mockTodo, id: mockId });
+    });
+
+    // Restore the original fetch function
+    global.fetch.mockRestore();
   });
 });
